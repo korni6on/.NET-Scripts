d8e0a1e3d08f5e07042ae83ec05b3dcf557b7c9c
Ivelin Kostov <korni6on@users.noreply.github.com>
2016-01-17 16:24:12 +0200

    Create AddHotSpotToIMageMap.cs

---
+++ ASP/AddHotSpotToIMageMap.cs
@@ -0,0 +1,7 @@
+RectangleHotSpot hs = new RectangleHotSpot(); 
+hs.Left = 10; 
+hs.Top = 10; 
+hs.Right = 200; 
+hs.Bottom = 100; 
+hs.NavigateUrl = "https://www.google.com/"; 
+ImageMap1.HotSpots.Add(hs);
